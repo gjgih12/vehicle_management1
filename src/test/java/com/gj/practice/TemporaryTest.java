1fde063a828caecf97a2fd49e6cc6545bcd5914e
“yougengjian <gengjian01@sinochem.com>
2020-09-26 23:46:45 +0800

    test

--- src/test/java/com/gj/practice/TemporaryTest.java
+++ src/test/java/com/gj/practice/TemporaryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -226,10 +227,122 @@ public class TemporaryTest {
     @Test
     public void test10(){
 
-        String str = "dwd2e/e2d/e3.mp4";
+        String str = "D:\\miniodata\\test\\temp\\20200926\\gengjian.mp4";
 
-        System.out.println(str.substring(str.lastIndexOf("/")+1));
+        InputStream is = null;
+        FileOutputStream fos = null;
+        try {
+            String filePath = str.substring(0,str.lastIndexOf("."));
+            is = new FileInputStream("C:\\Users\\Administrator\\Desktop\\eleProdData\\111.mp4");
+
+            File file = new File(filePath);
+            if(!file.exists()){
+                file.mkdirs();
+            }
+            fos = new FileOutputStream(filePath+"/hhh.mp4");
+            byte[] b = new byte[1024];
+            while ((is.read(b)) != -1) {
+                fos.write(b);// 写入数据
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                is.close();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
+
+    /**
+     * 删除目录
+     */
+    @Test
+    public void test13(){
+        deleteAnyone("D:\\miniodata\\test\\temp\\20200926\\8dbdde60913696f2a7f80399d963adf8");
+    }
+
+
+    public static boolean deleteAnyone(String FileName){
+
+        File file = new File(FileName);//根据指定的文件名创建File对象
+
+
+        if ( !file.exists() ){  //要删除的文件不存在
+            System.out.println("文件"+FileName+"不存在，删除失败！" );
+            return false;
+        }else{ //要删除的文件存在
+
+            if ( file.isFile() ){ //如果目标文件是文件
+
+                return deleteFile(FileName);
+
+            }else{  //如果目标文件是目录
+                return deleteDir(FileName);
+            }
+        }
+    }
+
+
+    public static boolean deleteFile(String fileName){
+
+
+        File file = new File(fileName);//根据指定的文件名创建File对象
+
+        if (  file.exists() && file.isFile() ){ //要删除的文件存在且是文件
+
+            if ( file.delete()){
+                System.out.println("文件"+fileName+"删除成功！");
+                return true;
+            }else{
+                System.out.println("文件"+fileName+"删除失败！");
+                return false;
+            }
+        }else{
+
+            System.out.println("文件"+fileName+"不存在，删除失败！" );
+            return false;
+        }
+
+
+    }
+
+
+
+    public static boolean deleteDir(String dirName){
+
+        if ( dirName.endsWith(File.separator) )//dirName不以分隔符结尾则自动添加分隔符
+            dirName = dirName + File.separator;
+
+        File file = new File(dirName);//根据指定的文件名创建File对象
+
+        if ( !file.exists() || ( !file.isDirectory() ) ){ //目录不存在或者
+            System.out.println("目录删除失败"+dirName+"目录不存在！" );
+            return false;
+        }
+
+        File[] fileArrays = file.listFiles();//列出源文件下所有文件，包括子目录
+
+
+        for ( int i = 0 ; i < fileArrays.length ; i++ ){//将源文件下的所有文件逐个删除
+
+            deleteAnyone(fileArrays[i].getAbsolutePath());
+
+        }
+
+
+        if ( file.delete() )//删除当前目录
+            System.out.println("目录"+dirName+"删除成功！" );
+
+
+        return true;
+
+    }
+
 
 }
