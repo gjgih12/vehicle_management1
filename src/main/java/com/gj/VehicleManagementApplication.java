fdc9e4ac35fb2a7a32a4f9885f3f79ab40cdf761
gengjian <gengjian01@sinochem.com>
2020-10-13 18:44:10 +0800

    swagger

--- src/main/java/com/gj/VehicleManagementApplication.java
+++ src/main/java/com/gj/VehicleManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//@EnableOpenApi
 //@EnableScheduling
 @SpringBootApplication
 //@EnableEncryptableProperties //开启加密注解
